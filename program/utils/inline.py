38ec46dd90cd51d95c1e6bb7288101ea0d05c776
CUTE_BOY <124450535+XcuteboyX@users.noreply.github.com>
2023-03-22 12:59:14 +0530

    create inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -16,8 +16,8 @@ def stream_markup(user_id):
       InlineKeyboardButton(text="‣‣I", callback_data=f'set_skip | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/Team_Bot_Support"),
-      InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/team_bot_update"),
+      InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/X_TEAM_IMMORTALS_X"),
+      InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/tHE_update_SILENCER"),
     ],
     [
       InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data="set_close"),
